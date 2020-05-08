6927ee628f499784e5c7fcff7b6943e03fd0b035
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2020-05-08 11:06:23 +0900

    6章まで

--- sample.rb
+++ sample.rb
@@ -1,22 +1,65 @@
-class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
-    @chainring = chainring
-    @cog = cog
-    @rim = rim
-    @tire = tire
+class Bicycle
+  attr_reader :size, :chain, :tire_size
+  def initialize(args={})
+    @size      = args[:size]
+    @chain     = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args)
   end
 
-  def ratio
-    chainring / cog.to_f
+  def post_initialize(args)
+    nil
   end
 
-  def gear_inches
-    ratio * (rim + (tire * 2))
+  def default_chain
+    "10-speed"
+  end
+
+  def default_tire_size
+    raise NotImplementedError
+          "This #{self.class} cannot respond to:"
+  end  
+ 
+  def spares
+    { tire_size: tire_size,
+      chain: chain}.merge(local_spares)
+  end
+
+  def local_spares
+    nil
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+class RoadBike < Bicycle 
+  attr_reader :tape_color
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+    super(args)
+  end
+ 
+  def local_spares
+    {tape_color: tape_color}
+  end
 
+  def default_tire_size
+    '23'
+  end
+end
+
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+  def post_initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
+    super(args)
+  end
+
+  def local_spares
+    {rear_shock: rear_shock}
+  end
+
+  def default_tire_size
+    '2.1'
+  end
+end
 
